a62c31f83a63865a4e6d0aafaed093dd87f4075e
VinothShanmugam <31066524+vinooMj@users.noreply.github.com>
2021-09-06 21:54:56 +0530

    Update and rename practiceBasic.java to practiceBasicProblemSol.java

--- practiceBasicProblemSol.java
+++ practiceBasicProblemSol.java
@@ -12,3 +12,15 @@ public boolean parrotTrouble(boolean talking, int hour) {
     return false;
   }
 }
+
+
+#Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+public boolean makes10(int a, int b) {
+   int sum = a+ b;
+  if(a == 10 || b == 10 || sum == 10 ){
+    return true;
+  }else{
+    return false;
+  }
+}
+
