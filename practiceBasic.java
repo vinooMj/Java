e28bcc5d19fd6e490fe1659d74751f706c63a0be
VinothShanmugam <31066524+vinooMj@users.noreply.github.com>
2021-09-06 21:41:39 +0530

    Create practiceBasic.java

---
+++ practiceBasic.java
@@ -0,0 +1,14 @@
+#practice problem beginers
+#We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
+#We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+
+public boolean parrotTrouble(boolean talking, int hour) {
+  if(talking & hour < 7){
+    return true;
+  }else if(talking & hour >20){
+    return true;
+  }else{
+    return false;
+  }
+}
