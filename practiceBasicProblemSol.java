1e2f4be7f2080557cdf0486d5d399d63bc084eaf
VinothShanmugam <31066524+vinooMj@users.noreply.github.com>
2021-09-13 22:36:24 +0530

    Update practiceBasicProblemSol.java

--- practiceBasicProblemSol.java
+++ practiceBasicProblemSol.java
@@ -49,4 +49,18 @@ public String notString(String str) {
   }
 }
 
+#Given a string, return a new string where the first and last chars have been exchanged.
+#frontBack("code") → "eodc"
+#frontBack("a") → "a"
+#frontBack("ab") → "ba"
+  
+  public String frontBack(String str) {
+  if(str.length() <=1)
+  return str;
+  
+  String mid = str.substring(1, str.length()-1);
+  return str.charAt(str.length()-1) +mid +str.charAt(0);
+}
+
+
 
