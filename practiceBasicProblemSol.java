f34c0fef8df16bd6c0380c2a11662a12c11acd6d
VinothShanmugam <31066524+vinooMj@users.noreply.github.com>
2021-09-13 22:48:58 +0530

    Update practiceBasicProblemSol.java

--- practiceBasicProblemSol.java
+++ practiceBasicProblemSol.java
@@ -62,5 +62,23 @@ public String notString(String str) {
   return str.charAt(str.length()-1) +mid +str.charAt(0);
 }
 
+#Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+
+#front3("Java") → "JavJavJav"
+#front3("Chocolate") → "ChoChoCho"
+#front3("abc") → "abcabcabc"
+  
+  public String front3(String str) {
+  String first;
+  if(str.length() >= 3){
+    first = str.substring(0 , 3);
+    
+  }else{
+    first = str;
+  }
+  return first+first+first
+}
+
 
 
